9ff67c55bed3e4914f686ef334d089887f4b50f4
Eddie Brazier <edwinbrazier01@gmail.com>
2020-03-08 17:22:08 -0400

    working on initial code structure

---
+++ Siren Rap/Assets/Scripts/MusicBeat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BeatType
+{ 
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class MusicBeat : MonoBehaviour
+{
+    //the type of beat this beat is
+    public BeatType beatType;
+
+    //the distance the beat will move from its' spawn point to the end point
+    public float moveDistance;
+
+    //the time it will take tehe beat to reach the end of the tail
+    public float moveTime;
+
+    //the amount of distance to increment the beat by each frame
+    private float moveIncrement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the distance to increment the beat by each frame
+        moveIncrement = (moveDistance / moveTime) * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //move to the right based on increment distance every frame
+        Vector3 tempPos = transform.position;
+        tempPos.x += moveIncrement;
+        transform.position = tempPos;
+    }
+}
---
+++ Siren Rap/Assets/Scripts/SongManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    //Dictionary of beats used for the song (easy mode)
+    [Tooltip ("These are the beats for easy mode, with the float representing the time in seconds into the game that the beat will spawn")]
+    public Dictionary<BeatType,float> easyBeats;
+
+    [Tooltip("These are the beats for normal mode, with the float representing the time in seconds into the game that the beat will spawn")]
+    public Dictionary<BeatType, float> normalBeats;
+
+    [Tooltip("These are the beats for hard mode, with the float representing the time in seconds into the game that the beat will spawn")]
+    public Dictionary<BeatType, float> hardBeats;
+
+    //the player's score
+    private int playerScore;
+
+    //the audio source for the level's song
+    private AudioSource levelSong;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
