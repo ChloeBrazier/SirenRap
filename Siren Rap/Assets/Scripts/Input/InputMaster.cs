5a2fef275315769cc5353074649505ee1868f6f0
Eddie Brazier <edwinbrazier01@gmail.com>
2020-03-09 00:14:01 -0400

    messing around with the new input system

---
+++ Siren Rap/Assets/Scripts/Input/InputMaster.cs
@@ -0,0 +1,277 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputMaster : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputMaster()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""Test"",
+            ""id"": ""383957f4-231d-445f-84bc-5d5bf9cad049"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""074655cb-afdf-47e2-b341-be6648c6bf40"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""DPad"",
+                    ""id"": ""6cd7bb28-180f-4082-8449-2455491003d4"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": ""Hold(duration=0.2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0a91f50b-e8f0-4cf3-a078-6f81e2127a38"",
+                    ""path"": ""<Gamepad>/dpad/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""b2c46f39-4495-4d14-a061-2638e181df07"",
+                    ""path"": ""<Gamepad>/dpad/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""ef0a4ef6-ad0e-4376-8631-cb3262bea888"",
+                    ""path"": ""<Gamepad>/dpad/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""982eec16-0610-4905-8125-0f919729639f"",
+                    ""path"": ""<Gamepad>/dpad/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""282778d7-5b83-4980-8d02-d52ba3896f14"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": ""Hold(duration=0.2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0ae80365-9557-4f91-8133-e13bcfd48d63"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""9299a82b-4eb3-4de2-beaf-8ad006b24fe1"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""6470a317-1791-447d-86ff-f31c62a10922"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""27eb9d2f-7bc0-4412-87bb-ea7814a24745"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard and Mouse"",
+            ""bindingGroup"": ""Keyboard and Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Test
+        m_Test = asset.FindActionMap("Test", throwIfNotFound: true);
+        m_Test_Movement = m_Test.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Test
+    private readonly InputActionMap m_Test;
+    private ITestActions m_TestActionsCallbackInterface;
+    private readonly InputAction m_Test_Movement;
+    public struct TestActions
+    {
+        private @InputMaster m_Wrapper;
+        public TestActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Test_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Test; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(TestActions set) { return set.Get(); }
+        public void SetCallbacks(ITestActions instance)
+        {
+            if (m_Wrapper.m_TestActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_TestActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_TestActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_TestActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_TestActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public TestActions @Test => new TestActions(this);
+    private int m_KeyboardandMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardandMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardandMouseSchemeIndex == -1) m_KeyboardandMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard and Mouse");
+            return asset.controlSchemes[m_KeyboardandMouseSchemeIndex];
+        }
+    }
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    public interface ITestActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
---
+++ Siren Rap/Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    //input master
+    private InputMaster controls;
+
+    //colored squares for input testing
+    public GameObject redSquare;
+    public GameObject blueSquare;
+    public GameObject purpleSquare;
+    public GameObject greenSquare;
+
+    //pre-placed squares for input testing
+    public GameObject upSquare;
+    public GameObject downSquare;
+    public GameObject leftSquare;
+    public GameObject rightSquare;
+
+    private void Awake()
+    {
+        //initialize controls
+        controls = new InputMaster();
+
+        //hook up controls
+        controls.Test.Movement.started += context => SetBlock(context.ReadValue<Vector2>());
+        controls.Test.Movement.canceled += context => ResetBlock(context.action.activeControl.displayName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBlock(Vector2 blockDirection)
+    {
+        if(blockDirection.x < 0)
+        {
+            //replace left black square with purple sqaure
+            Instantiate(purpleSquare, leftSquare.transform.position, Quaternion.identity);
+
+            //disable left square
+            leftSquare.SetActive(false);
+
+        }
+        else if(blockDirection.x > 0)
+        {
+            //replace right black square with blue sqaure
+            Instantiate(blueSquare, rightSquare.transform.position, Quaternion.identity);
+
+            //disable right square
+            rightSquare.SetActive(false);
+        }
+        else if(blockDirection.y > 0)
+        {
+            //replace top black square with red sqaure
+            Instantiate(redSquare, upSquare.transform.position, Quaternion.identity);
+
+            //disable top square
+            upSquare.SetActive(false);
+        }
+        else if(blockDirection.y < 0)
+        {
+            //replace bottom black square with green sqaure
+            Instantiate(greenSquare, downSquare.transform.position, Quaternion.identity);
+
+            //disable bottom square
+            downSquare.SetActive(false);
+        }
+    }
+
+    public void ResetBlock(string pressedButton)
+    {
+        Debug.Log("ToString contents: " + pressedButton);
+
+        if(pressedButton.Contains("Up"))
+        {
+            //enable top black square
+            upSquare.SetActive(true);
+        }
+        else if(pressedButton.Contains("Left"))
+        {
+            //enable left black square
+            leftSquare.SetActive(true);
+        }
+        else if (pressedButton.Contains("Right"))
+        {
+            //enable right square
+            rightSquare.SetActive(true);
+        }
+        else if (pressedButton.Contains("Down"))
+        {
+            //enable right square
+            downSquare.SetActive(true);
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
